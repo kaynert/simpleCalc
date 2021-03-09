d0730af8872285ae1871829ebf31d72346100f58
kaynert <kofinertey@gmail.com>
2021-03-09 02:37:14 +0000

    JS file to emulate basic calculator functions

---
+++ calculator.js
@@ -0,0 +1,34 @@
+function ADD( a, b) {
+
+    console.log(a + b);
+    alert( a + b );
+   }
+
+
+
+function SUBTRACT(a ,b ) {
+
+   console.log (a - b);
+   alert (a - b);
+
+}
+
+function DIVIDE (a, b) {
+
+   console.log ( a /b);
+   alert (a / b);
+
+
+}
+
+function MULTIPLY (a,b ) {
+console.log (a * b);
+alert ( a * b);
+
+}
+
+// test cases
+ADD( 5, 8);
+SUBTRACT( 9,2);
+DIVIDE ( 4, 5);
+MULTIPLY ( 5 , 6);
